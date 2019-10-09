c397c8cd13691c33366dddb4b670ee3402e2b8d9
Richard Priestley <r.priestley@comanduk.co.uk>
2019-10-09 11:11:02 +0100

    Fixed test cases

--- karma.conf.js
+++ karma.conf.js
@@ -5,6 +5,11 @@ const OUTPUT_DIR = process.env.TEST_UNDECLARED_OUTPUTS_DIR;
 
 module.exports = function(config) {
   config.set({
+    plugins: [
+      "karma-junit-reporter",
+      "karma-coverage-istanbul-instrumenter",
+      "karma-coverage-istanbul-reporter"
+    ],
     reporters: ['junit', 'coverage-istanbul'],
     junitReporter: {
       outputDir: path.join(OUTPUT_DIR, "junit"),

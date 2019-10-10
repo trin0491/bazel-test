13bbebaf04df8edef45830c2aef97ef9685165c2
Richard Priestley <r.priestley@comanduk.co.uk>
2019-10-10 17:57:57 +0100

    Rollup as UMD bundle

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -2,6 +2,10 @@ const node = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 
 module.exports = {
+  output: {
+    name: "core",
+    format: "umd"
+  },
   plugins: [
     node({
       mainFields: ['browser', 'es2015', 'module', 'jsnext:main', 'main'],

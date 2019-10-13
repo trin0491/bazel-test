339f50de763c61b76102784efce14012fe4e3c2d
Richard Priestley <r.priestley@comanduk.co.uk>
2019-10-13 18:50:34 +0100

    Externalise lodash from ui bundles

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -4,8 +4,14 @@ const commonjs = require('rollup-plugin-commonjs');
 module.exports = {
   output: {
     name: "ui",
-    format: "umd"
+    format: "umd",
+    globals: {
+      "lodash": "_"
+    }
   },
+  external: [
+    'lodash'
+  ],
   plugins: [
     node({
       mainFields: ['browser', 'es2015', 'module', 'jsnext:main', 'main'],

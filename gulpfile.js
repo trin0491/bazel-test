4238d6d65b95f64822ecd00bbe15cb1a77bcc3e4
Richard Priestley <r.priestley@comanduk.co.uk>
2019-11-18 16:07:40 +0000

    Updated copy job to use install tree

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const BAZEL_BIN="./dist/bin";
 
 gulp.task('copy:core', () => {
   return gulp.src(`${BAZEL_BIN}/packages/core/npm_package/**/*`)
-    .pipe(gulp.dest('./dist/install/core', {dirMode: "0755", mode: "0644"}));
+    .pipe(gulp.dest('../install/common/core', {dirMode: "0755", mode: "0644"}));
 });
 
 gulp.task('build', gulp.series('copy:core', (done) => {
